520b740ba115139c754d78dfaf406f200fc2668a
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-16 13:04:57 +0100

    Milestone 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 function App() {
 
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+  function ProductList({ products }) {
+    return <ul>
+      {
+        products.map((product, i) => (
+          <li key={i}>
+            <strong>Prodotto:</strong> <span>{product.name}</span> <br />
+            <strong>Prezzo:</strong> <span>{product.price}€</span>
+          </li>
+        ))
+      }
+    </ul>
+  }
+
   return (
     <>
+      <h1>Carrello della spesa</h1>
 
+      <div>
+        <ProductList
+          products={products}
+        />
+      </div>
     </>
   )
 }
