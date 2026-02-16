09ecb12121927fd3dcc67ee42fc947a466e34249
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-16 13:56:29 +0100

    Milsestone 2 dome

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
 
   const products = [
@@ -13,21 +15,56 @@ function App() {
         products.map((product, i) => (
           <li key={i}>
             <strong>Prodotto:</strong> <span>{product.name}</span> <br />
-            <strong>Prezzo:</strong> <span>{product.price}€</span>
+            <strong>Prezzo:</strong> <span>{product.price}€</span> <br />
+            <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
           </li>
         ))
       }
     </ul>
   }
 
+
+  const [addedProducts, setAddedProducts] = useState([])
+
+  function addToCart(product) {
+    const productFound = addedProducts.find((p) => p.name === product.name)
+
+    !productFound ?
+      setAddedProducts(curr => {
+        return [...curr, { ...product, quantity: 1 }]
+      }) : ""
+  }
+
+
   return (
     <>
       <h1>Carrello della spesa</h1>
 
       <div>
+        <h4>Prodotti 📝</h4>
+
         <ProductList
           products={products}
         />
+
+        {addedProducts.length ?
+          <div>
+            <h4>Carrello 🛒</h4>
+            <ul>
+              {
+                addedProducts.map((product, i) => (
+                  <li key={i}>
+                    <strong>Prodotto:</strong> <span>{product.name}</span> <br />
+                    <strong>Prezzo:</strong> <span>{product.price}€</span> <br />
+                    <strong>Quantità:</strong> <span>{product.quantity}</span>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+          :
+          <h4>Il carrello è vuoto</h4>
+        }
       </div>
     </>
   )
